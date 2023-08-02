e9011eb6b6b959ede42d721e878888f85decb4c0
DylanJaveri <140866137+DylanJaveri@users.noreply.github.com>
2023-08-02 13:16:15 +0900

    a

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import random
 import json
 import pickle
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 import tensorflow as tf
 import nltk
