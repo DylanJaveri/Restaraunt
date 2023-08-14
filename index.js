e3f1c99c116cace8af2711d9d091d6f99a4c9a85
DylanJaveri <140866137+DylanJaveri@users.noreply.github.com>
2023-08-14 18:40:22 +0900

    f

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-// Selecting the html class and 
-// convert it to an object
- 
-const sharebtn =
-    document.querySelector('.sharebtn')
- 
-// Creating a bool variable for changing
-// the image of share button
-let bool = 0
- 
-// Adding an event listener
-sharebtn.addEventListener('click', () => {
- 
-    // As we clicked the mouse over
-    // the share button the bool value.
-    //  get flipped and then working of
-    // if-else loop get starts
-    bool = !bool
-     
-    if (bool == 0) {
-        sharebtn.innerHTML =
-            '<i class="far fa-share-square"></i>'
-    } else {
-        sharebtn.innerHTML =
-            '<i class="fas fa-times"></i>'
-    }
-})
+// Get all share buttons
+const shareButtons = document.querySelectorAll('.share-button');
+
+// Add click event listener to each button
+shareButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      // Get the URL of the current page
+      const url = window.location.href;
+
+      // Get the social media platform from the button's class name
+      const platform = button.classList[1];
+
+      // Set the URL to share based on the social media platform
+      let shareUrl;
+      switch (platform) {
+         case 'facebook':
+         shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+         break;
+         case 'twitter':
+         shareUrl = `https://twitter.com/share?url=${encodeURIComponent(url)}`;
+         break;
+         case 'linkedin':
+         shareUrl = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}`;
+         break;
+         case 'pinterest':
+         shareUrl = `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(url)}`;
+         break;
+         case 'reddit':
+         shareUrl = `https://reddit.com/submit?url=${encodeURIComponent(url)}`;
+         break;
+         case 'whatsapp':
+         shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`;
+         break;
+      }
+
+      // Open a new window to share the URL
+      window.open(shareUrl, '_blank');
+   });
+});
